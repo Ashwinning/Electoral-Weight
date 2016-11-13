7b581e727f7839e0a7948e683f622f36fb251906
Ashwin Sinha <ash@ds.gg>
2016-11-13 23:29:48 +0530

    candidates per state

---
+++ votesbystate.py
@@ -0,0 +1,8 @@
+'''
+Sort votes by state from votes.txt
+Data scraped from http://www.politico.com/2016-election/results/map/president
+'''
+
+candidates = [4,6,4,8,5,22,4,4,4,6,3,5,8,4,3,10,4,6,13,4,4,4,6,9,7,5,5,4,6,5,9,8,4,3,6,5,3,4,5,5,7,4,7,4,10,6,5,7,5,7,6]
+
+file = open("votes.txt", "r")
