2e9e74308b8c38217a31c2e95cf14866af452519
Ashwin Sinha <ash@ds.gg>
2016-11-13 23:54:53 +0530

    Calculated votes by state

--- votesbystate.py
+++ votesbystate.py
@@ -5,19 +5,16 @@ Data scraped from http://www.politico.com/2016-election/results/map/president
 
 candidates = [4,6,4,8,5,22,4,4,4,6,3,5,8,4,3,10,4,6,13,4,4,4,6,9,7,5,5,4,6,5,9,8,4,3,6,5,3,4,5,5,7,4,7,4,10,6,5,7,5,7,6]
 
-#votesFiles = open("votes.txt", "r")
+votes = open("votes.txt", "r")
+votesByState = open("votesbystate.txt", "w")
 
+def ClearCommasAndReturnInt(text):
+    return int(text.replace(',', ''))
 
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
+votes.readline() #skip first line
 
-print ('Number of votes ' + str(file_len("votes.txt")))
-
-total = 0
 for num in candidates:
-    total += num
-
-print ('number of votes '+ str(total))
+    totalVotesInState = 0
+    for i in range(1,num):
+        totalVotesInState += ClearCommasAndReturnInt(votes.readline())
+    votesByState.write(str(totalVotesInState)+'\n')
